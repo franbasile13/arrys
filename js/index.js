be038395b1f80b54e1d03971bc5f9f452089b7d4
Basile Franco <francobasile@outlook.com>
2022-08-09 21:50:22 -0300

    arrays

---
+++ js/index.js
@@ -0,0 +1,26 @@
+let autos = 'Volkswagen,Fiat,Renault,Ford,Chevrolet,Dodge';
+
+let array = autos.split(",")
+    array;
+    
+array.length;
+array[0];
+array [1];
+array[array.length-1];
+
+autos.push("Ferrari");
+autos; 
+autos.push("Lotus", "Nissan");
+autos;
+
+let removerItem = autos.pop();
+autos; 
+removerItem; 
+
+autos.unshift("Suzuki");
+autos; 
+removerItem; 
+
+
+
+
